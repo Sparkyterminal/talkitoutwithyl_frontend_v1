2f936b846813c53baea98bd78c283265bc518df5
sparkyterminal <shashankau17@gmail.com>
2025-09-02 11:05:36 +0530

    final changes and build

--- config.js
+++ config.js
@@ -1,27 +1,27 @@
 /* eslint-disable no-undef */
-export const API_BASE_URL = "http://localhost:4042/";
+// export const API_BASE_URL = "http://localhost:4042/";
+// export const BASE_URL = "/";
+// export const SOCKET_URL = "http://localhost:4042";
+// export const BASE_LINK = "/";
+// export const SOCKET_PATH = "/";
+// export const DEFAULT_IP_ADDRESS = "127.0.0.1";
+// export const AUTHOR_NAME = "talkitoutwithyl";
+// export const JWT_SECRET = "Secret!25!keerthana"
+// export const LOGGER = process.env.NODE_ENV === 'development';
+// export const ROLES = {
+//   ADMIN: 'ADMIN',
+//   USER: 'USER'
+// }
+export const API_BASE_URL = "https://www.talkitoutwithyl.com/api/";
 export const BASE_URL = "/";
-export const SOCKET_URL = "http://localhost:4042";
+export const SOCKET_URL = "https://www.talkitoutwithyl.com/api/";
 export const BASE_LINK = "/";
 export const SOCKET_PATH = "/";
 export const DEFAULT_IP_ADDRESS = "127.0.0.1";
 export const AUTHOR_NAME = "talkitoutwithyl";
-export const JWT_SECRET = "Secret!25!keerthana"
-export const LOGGER = process.env.NODE_ENV === 'development';
+export const JWT_SECRET = "Secret!25!keerthana";
+export const LOGGER = process.env.NODE_ENV === "development";
 export const ROLES = {
-  ADMIN: 'ADMIN',
-  USER: 'USER'
-}
-// export const API_BASE_URL = "https://www.nuvohosting.org/";
-// export const BASE_URL = "/";
-// export const SOCKET_URL = "https://www.nuvohosting.org";
-// export const BASE_LINK = "/";
-// export const SOCKET_PATH = "/";
-// export const DEFAULT_IP_ADDRESS = "127.0.0.1";
-// export const AUTHOR_NAME = "nuvohosting";
-// export const JWT_SECRET = "secret!25!Nuvo";
-// export const LOGGER = process.env.NODE_ENV === "development";
-// export const ROLES = {
-//   ADMIN: "ADMIN",
-//   USER: "USER",
-// };
+  ADMIN: "ADMIN",
+  USER: "USER",
+};
